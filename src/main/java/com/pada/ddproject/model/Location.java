e037177149364c04ab6fe9adb83df2564474d560
Piotr Adamski <p.adamski@reply.com>
2024-06-27 21:46:17 +0200

    add base classes in domain

---
+++ src/main/java/com/pada/ddproject/model/Location.java
@@ -0,0 +1,12 @@
+package com.pada.ddproject.model;
+
+
+import java.util.List;
+
+class Location {
+    private String name;
+    private String description;
+    private String continent;
+    private String country;
+    private List<Product> bannedProducts;
+}
